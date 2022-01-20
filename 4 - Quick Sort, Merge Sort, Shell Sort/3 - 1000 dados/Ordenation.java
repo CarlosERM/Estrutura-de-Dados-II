fe0f7497052c34e4b18efc5db08a6e05c08cfff7
Carlos <carlos.eduardo@estudante.ifgoiano.edu.br>
2022-01-20 12:43:11 -0300

    Recomeço

---
+++ 4 - Quick Sort, Merge Sort, Shell Sort/3 - 1000 dados/Ordenation.java
@@ -0,0 +1,9 @@
+public class Ordenation {
+
+    static void printArray(int arr[], int size) {
+        int i;
+        for (i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+            System.out.println();
+    }
+}
